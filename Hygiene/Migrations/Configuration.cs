69d07e017b69ae9e3ca0f47ffbe0022c994c9253
JMS11 <palejames@gmail.com>
2015-07-08 12:17:40 +0100

    Cleaned up code and moved null check functions to Utility

--- Hygiene/Migrations/Configuration.cs
+++ Hygiene/Migrations/Configuration.cs
@@ -1,7 +1,6 @@
 namespace Hygiene.Migrations
 {
-    using Hygiene.Models;
-    //using Hygiene.Utility;
+    using Hygiene.Utility;
     using HygieneBO;
     using System;
     using System.Data.Entity;
@@ -374,23 +373,6 @@ namespace Hygiene.Migrations
                    
                     
 
-
-            //foreach (XElement xe in xmlDoc.Descendants("EstablishmentDetail")) {
-            //    context.Restaurants.AddOrUpdate(r => r.BusinessName, 
-            //        new Restaurant {
-            //        FHRSID = (int)xe.Element("FHRSID"),
-            //        BusinessName = xe.Element("BusinessName").Value,
-            //        AddressLine1 = UtilityFunctions.Addr1(xe),
-            //        AddressLine2 = UtilityFunctions.Addr2(xe),
-            //        PostCode = UtilityFunctions.PstCde(xe),
-            //        LocalAuthorityName = UtilityFunctions.AuthName(xe),
-            //        ImagePath = UtilityFunctions.Image(xe)
-                    
-            //        },
-
-            //        )
-
-            //}
 
             //context.Restaurants.AddOrUpdate(r => r.BusinessName,
             //    new Restaurant {},
